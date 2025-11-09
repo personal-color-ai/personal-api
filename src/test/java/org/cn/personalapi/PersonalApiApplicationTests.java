4cac1440f749fcea8cc4315846396886db07e159
heo <=>
2025-11-09 14:16:59 +0900

    init

---
+++ src/test/java/org/cn/personalapi/PersonalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cn.personalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
